ee0e16408f1ec57bd88c2b4b4f65dc1ceca8333a
Bill Brubaker <brubaker.bill@gmail.com>
2017-03-15 12:58:23 -0500

    working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var sql = require('mysql');
 var connection = sql.connection({
-    host     :  'localhost',
+    host     :  'localhost:4050',
     user     :  'root',
     password : 'Pa$$word12',
-    database : 'my_db'
+    database : 'mysql'
 });
